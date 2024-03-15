a46e2f7d64695a278c8c2804089bb5c7fb96b510
HB <boradhetvi214@gmail.com>
2024-03-15 20:17:00 +0530

    HB | to NJ

--- App.js
+++ App.js
@@ -1,30 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-// import Camera from './Camera';
-// import Theme from './Theme'
-// import SettingNotification from './SettingNotification';
-// import Notification from './Notification';
-// import Setting from './Setting';
-// import DetailBudget from './DetailBudget';
-// import Create_Budget from './Create_Budget';
-// import Theme from './Theme';
+import Camera from './Camera';
+import SettingNotification from './SettingNotification';
+import Notification from './Notification';
+import Setting from './Setting';
+import DetailBudget from './DetailBudget';
+import Create_Budget from './Create_Budget';
+import Theme from './Theme';
 import Budget from './Budget';
-// import Profile from './Profile';
-// import Remove_Budget from './Remove_Budget';
-// import Account from './Account';
+import Profile from './Profile';
+import Remove_Budget from './Remove_Budget';
+import Account from './Account';
 
 export default function App() {
   return (
   <View>
     {/* <Camera/> */}
-    {/* <Theme/> */}
+    <Theme/>
     {/* <SettingNotification/> */}
     {/* <Notification/> */}
     {/* <Setting/> */}
     {/* <DetailBudget/> */}
     {/* <Create_Budget/> */}
     {/* <Theme/> */}
-    <Budget/>
+    {/* <Budget/> */}
     {/* <Profile/>
     */}
     {/* <Remove_Budget/> */}
